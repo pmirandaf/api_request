3b11859ff99a5e2107469c0018a7e9862d57fec3
Pablo Miranda <pablo.miranda.oliveira@gmail.com>
2023-07-06 22:27:55 -0300

    http api request

---
+++ aula1.py
@@ -0,0 +1,24 @@
+
+#Consumindo API do Github
+
+import requests
+
+username = input('Qual seu nome? ')
+
+url = f'https://api.github.com/users/{username}'
+
+response = requests.get(url)
+data = response.json()  
+
+
+if response.status_code == 200:
+    # print(data)
+    print(f'ID: {data["id"]}')
+    print(f'Nome Completo: {data["name"]}')
+    print(f'Location: {data["location"]}')
+else:
+        print('Error')
+
+
+
+# print('Github Users')
